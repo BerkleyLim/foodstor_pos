5fd5ffa3957d304de9633d0c7d97b970c48495bf
berkleylim <berkleylim16@gmail.com>
2023-09-08 21:45:44 +0900

    ci: back-end setting
    -백엔드 기본 설치

---
+++ food-back/src/main/java/com/berkley/food/store/pos/machine/foodback/FoodBackApplication.java
@@ -0,0 +1,13 @@
+package com.berkley.food.store.pos.machine.foodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBackApplication.class, args);
+	}
+
+}
---
+++ food-back/src/test/java/com/berkley/food/store/pos/machine/foodback/FoodBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkley.food.store.pos.machine.foodback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
