04e3326e204d73d5fea03f511a4104dcce909e66
berkleylim <berkleylim16@gmail.com>
2023-09-11 23:39:00 +0900

    test: my-batis 연동 테스트 - 맵퍼 파일을 이용하여 테스트

---
+++ food-back/src/main/java/com/berkley/food/store/pos/machine/foodback/mapper/FoodMapper.java
@@ -0,0 +1,9 @@
+package com.berkley.food.store.pos.machine.foodback.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface FoodMapper {
+}
