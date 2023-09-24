2acc3a4caf47085a205f746b6e2b1db945ed1be7
berkleylim <berkleylim16@gmail.com>
2023-09-24 16:32:05 +0900

    feat: 음식점 포스 프로그램 기능 추가 - 화면단 추가 - recoil foodState로 state 값 저장 - 화면단 recoil를 이용하여 기능 추가

--- food-front/src/component/posfood/PosFood.tsx
+++ food-front/src/component/posfood/PosFood.tsx
@@ -2,88 +2,47 @@ import React from 'react'
 import { Button, Card, CardBody, Col, Row } from 'reactstrap'
 import styles from "../index.module.scss"
 import Header from '../Header'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { foodState } from "../../recoil/state/foodState"
+import { userBankInfoState } from "../../recoil/state/userBankInfoState"
 
 const PosFood = () => {
+  const food = useRecoilValue(foodState);
+  const [bankInfo, setBankInfo] = useRecoilState(userBankInfoState);
+
+  // 구입 시 선택
+  const purchaseFood = (f:any | Object) => {
+    let tempBankInfo = {
+      bno: bankInfo?.bno,
+      userMoney: bankInfo.userMoney - f?.foodPrice - f?.VAT
+    }
+    setBankInfo(tempBankInfo)
+  }
   return (
     <div>
       <Header />
       <div className={`${styles?.container}`}>
         <h1>음식 메뉴</h1>
-        <div style={{ textAlign:"right" }}>
-          <Button onClick={() => {window.history.back()}}>뒤로가기</Button>
+        <div style={{ textAlign: "right" }}>
+          <Button onClick={() => { window.history.back() }}>뒤로가기</Button>
         </div>
-        <div>현재 잔액 : 200000원</div>
+        <div className={styles?.moneyTextInfo}>현재 잔액 : <span className={styles?.moneyText}>{bankInfo?.userMoney}</span>원</div>
         <Row className={`mt-5`}>
-          <Col md={4} className={`mb-5`}>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    장어 구이 : 5000원
-                  </Col>
-                  <Col>
-                  <Button>선택</Button>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={4} className={`mb-5`}>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    피자 : 11000원
-                  </Col>
-                  <Col>
-                    <Button>선택</Button>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={4} className={`mb-5`}>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    치즈스틱 : 3000원
-                  </Col>
-                  <Col>
-                    <Button>선택</Button>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={4} className={`mb-5`}>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    로스트 비프 : 15000원
-                  </Col>
-                  <Col>
-                    <Button>선택</Button>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={4} className={`mb-5`}>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    감자튀김 : 4000원
-                  </Col>
-                  <Col>
-                    <Button>선택</Button>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
+          {food?.map((f, index) =>
+            <Col md={4} key={index} className={`mb-5`}>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col>
+                      {f?.foodMenu} : {(f?.foodPrice + f?.VAT)}원
+                    </Col>
+                    <Col>
+                      <Button onClick={() => purchaseFood(f)}>선택</Button>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>)}
         </Row>
       </div>
 
---
+++ food-front/src/recoil/state/foodState.tsx
@@ -0,0 +1,39 @@
+import {atom} from 'recoil'
+
+export const foodState = atom({
+  key: 'food',
+  default: [
+    {
+        fno: 1
+      , foodMenu: '장어구이'
+      , foodPrice: 5000
+      , VAT: 500
+    },
+    {
+        fno: 2
+      , foodMenu: '피자'
+      , foodPrice: 10000
+      , VAT: 1000
+    },
+    {
+        fno: 3
+      , foodMenu: '치즈스틱'
+      , foodPrice: 3000
+      , VAT: 300
+    },
+    {
+        fno: 4
+      , foodMenu: '로스트비프'
+      , foodPrice: 15000
+      , VAT: 1500
+    },
+    {
+        fno: 5
+      , foodMenu: '감자튀김'
+      , foodPrice: 4000
+      , VAT: 400
+    },
+  ]
+});
+
+// https://velog.io/@juno7803/Recoil-Recoil-200-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0
