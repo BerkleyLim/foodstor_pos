cdebd960d2bc5b47e6704fc860b6add60aa90f44
berkleylim <berkleylim16@gmail.com>
2023-10-07 21:37:16 +0900

    test: data.sql 수정 - 현재, Mapper 테스트 용으로 지정 - insert 구문 일부 삭제 (프론트엔드에서 삽입한 DB 활용을 위해)

--- food-back/src/main/resources/data.sql
+++ food-back/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 -- 테스트용 DML 문
 -- insert into t_test (test_field) values ('테스트용');
 
--- 회원 정보 구문 : 프론트엔드 샘플 데이터용으로 추가
-insert into t_user
-(user_name, user_age, user_sex, user_phone, user_number, user_card_password)
-values ('Berkley', 29, '남', '01011111111', '880401', '1111');
+-- -- 회원 정보 구문 : 프론트엔드 샘플 데이터용으로 추가
+-- insert into t_user
+-- (user_name, user_age, user_sex, user_phone, user_number, user_card_password)
+-- values ('Berkley', 29, '남', '01011111111', '880401', '1111');
 
--- 회원 은행 계좌 관련 구문
-insert into t_account (uno, user_money) values (1, 100000);
+-- -- 회원 은행 계좌 관련 구문
+-- insert into t_account (uno, user_money) values (1, 100000);
 
 -- 음식 메뉴 insert 구문
 insert into t_food (food_menu, food_price, VAT) values ('장어덮밥',5000, 500);
@@ -17,11 +17,11 @@ insert into t_food (food_menu, food_price, VAT) values ('로스트비프',15000,
 insert into t_food (food_menu, food_price, VAT) values ('감자튀김',4000,400);
 
 
--- 로그 insert 구문
-insert into t_log 
-(uno, page_no, page_event_title, page_event_view, crt_time) 
-values 
-(1, 1, '로그인', '로그인 진입', now());
+-- -- 로그 insert 구문
+-- insert into t_log 
+-- (uno, page_no, page_event_title, page_event_view, crt_time) 
+-- values 
+-- (1, 1, '로그인', '로그인 진입', now());
 
 -- 회원 정보 테스트
 select * from t_user;
