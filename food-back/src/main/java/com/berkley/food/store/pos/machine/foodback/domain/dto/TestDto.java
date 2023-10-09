b8abf06580d49e281ac8922a2ee547bf1020eb57
berkleylim <berkleylim16@gmail.com>
2023-10-09 17:24:29 +0900

    remove: testDto 제거 - 현재 테스트 할 필요없어서 제거 - 자세한건 git commit message 이력사항 확인할 것

--- food-back/src/main/java/com/berkley/food/store/pos/machine/foodback/domain/dto/TestDto.java
+++
@@ -1,9 +0,0 @@
-package com.berkley.food.store.pos.machine.foodback.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class TestDto {
-  Long uno;
-  String testField;
-}
--- food-back/src/main/java/com/berkley/food/store/pos/machine/foodback/domain/vo/TestVo.java
+++
@@ -1,9 +0,0 @@
-package com.berkley.food.store.pos.machine.foodback.domain.vo;
-
-import lombok.Data;
-
-@Data
-public class TestVo {
-  Long uno;
-  String testField;
-}
