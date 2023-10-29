9379373175e6b0bb1571ef7f77e3611dfec8cbf1
berkleylim <berkleylim16@gmail.com>
2023-10-29 17:56:12 +0900

    fix: 계좌 정보 관련 API 연동 - React Query로 이용하여 계좌 입출금 및 계좌 조회 활성화

--- food-front/src/component/bank/Bank.tsx
+++ food-front/src/component/bank/Bank.tsx
@@ -1,63 +1,110 @@
 import Header from '../Header'
 import styles from '../index.module.scss'
 import { Button, Card, CardBody, Col, Row } from 'reactstrap'
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
-import { userBankInfoState } from "../../recoil/state/userBankInfoState"
-import { logInfoState } from '../../recoil/state/logInfoState'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { UserBankInfoVo, userBankInfoState } from "../../recoil/state/userBankInfoState"
+import { userInfoState } from '../../recoil/state/userInfoState'
+import { useMutation, useQueries } from 'react-query'
 
+// main 클래스
 const Bank = () => {
   const [bankInfo, setBankInfo] = useRecoilState(userBankInfoState);
-
-  /** 로그값 추가 */
-  const logInfo: any[] = useRecoilValue(logInfoState);
-  const setLogInfo = useSetRecoilState(logInfoState);
   const today = new Date();
 
+
+  const userInfo = useRecoilValue(userInfoState)
+  console.log(userInfo)
+
+  // 리액트 Query 작성 
+  // (결론 : 로그인 할때만 쓰자!, 실시간 갱신에 좋으나, 기기에 따라 로딩 속도가 오래 걸린다.)
+
+  // React Query에 사용할 객체 저장
+  const queries = [
+    {
+      queryKey: ['userAccount',1],
+      queryFn: () => {
+        fetchSelect();
+      }
+    }
+  ]
+
+  const selectQueries = useQueries(queries);
+
+  const changeMoney = useMutation(async (update:UserBankInfoVo) => {
+    const response = await fetch('http://localhost:8080/api/account/change', {
+      method: 'POST',
+      body: JSON.stringify(update),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return response
+  }, {
+    onSuccess: (data) => {
+      console.log(data)
+      console.log("성공")
+      fetchSelect();
+    }
+  });
+
+  // 공통으로 쓰기
+  const fetchSelect = () => {
+    const uno = (!!userInfo?.uno) ? userInfo?.uno : 0
+    fetch('http://localhost:8080/api/account/'+uno).then( 
+        (res) => res.json()
+      ).then( (res) => {
+        console.log(res)
+        setBankInfo(res)
+    }).catch((e) => console.log(e))
+  }
+
+
+  
+  if (selectQueries[0].isLoading) {
+    return <div>현재 회원 정보 계좌 금액 출력 중 입니다.</div>
+  }
+
+  if (selectQueries[0].isError) {
+    return <p>계좌 정보 불려오는 것이 실패하였습니다.</p>
+  }
+
   console.log(bankInfo)
-  const decrementMoney = () => {
-    let tempBankInfo = {
-      bno: bankInfo?.bno,
-      userMoney: bankInfo.userMoney - 100000
-    }
-    setBankInfo(tempBankInfo)
+  // 출금 금액 로직
+  const DecrementMoney = () => {
 
     /** 로그에 담는다 */
-    const createLog: any = {
-      lno: logInfo.length + 1,
+    const userBankInfoVo: UserBankInfoVo = {
+      // lno: logInfo.length + 1,
+      userMoney: bankInfo?.userMoney as number - 100000,
+      uno: userInfo?.uno,
       pageNo: 3,
       pageEventTitle: "계좌",
-      pageEventView: "출금 10만원, 남은 잔액 : " + (bankInfo.userMoney - 100000) + "원",
+      pageEventView: "출금 10만원, 남은 잔액 : " + (bankInfo?.userMoney as number - 100000) + "원",
       crtTime: JSON.stringify(today),
     }
 
-    setLogInfo(
-      [...logInfo,
-        createLog]
-    )
+    changeMoney.mutate(userBankInfoVo)
   }
+
+  // 입금 금액 로직
   const incrementMoney = () => {
-    let tempBankInfo = {
-      bno: bankInfo?.bno,
-      userMoney: bankInfo.userMoney + 100000
-    }
-    setBankInfo(tempBankInfo)
-
     /** 로그에 담는다 */
-    const createLog: any = {
-      lno: logInfo.length + 1,
-      uno: 1,
+    const userBankInfoVo: UserBankInfoVo = {
+      // lno: logInfo.length + 1,
+      userMoney: bankInfo?.userMoney as number + 100000,
+      uno: userInfo?.uno,
       pageNo: 3,
       pageEventTitle: "계좌",
-      pageEventView: "입금 10만원, 남은 잔액 : " + (bankInfo.userMoney + 100000) + "원",
+      pageEventView: "입금 10만원, 남은 잔액 : " + (bankInfo?.userMoney as number + 100000) + "원",
       crtTime: JSON.stringify(today),
     }
 
-    setLogInfo(
-      [...logInfo,
-        createLog]
-    )
+    changeMoney.mutate(userBankInfoVo)
+
   }
 
+
+  // setBankInfo(data);
   return (
     <div>
       <Header />
@@ -69,7 +116,8 @@ const Bank = () => {
         </div>
         <Card className={``}>
           <CardBody>
-            <div className={styles?.moneyTextInfo}>현재 잔액 : <span className={styles?.moneyText}>{bankInfo?.userMoney}</span>원</div>
+            <div className={styles?.moneyTextInfo}>현재 잔액 : <span className={styles?.moneyText}>{bankInfo?.userMoney as number}</span>원</div>
+            {/* <div className={styles?.moneyTextInfo}>현재 잔액 : <span className={styles?.moneyText}>{data?.userMoney as number}</span>원</div> */}
             <br /><br /><br />
             <span className={styles?.info}>※ 입출금은 100,000원 단위로 증감 합니다..</span>
             <br /><br />
@@ -78,7 +126,7 @@ const Bank = () => {
                 <Button className={`w-100`} onClick={() => incrementMoney()}>입금</Button>
               </Col>
               <Col sm={{ offset: 7, size: "auto" }}>
-                <Button className={`w-100`} onClick={() => decrementMoney()}>출금</Button>
+                <Button className={`w-100`} onClick={() => DecrementMoney()}>출금</Button>
               </Col>
             </Row>
           </CardBody>
