7c430c4131ed538242f276a6d179dadbf4be7587
berkleylim <berkleylim16@gmail.com>
2023-10-29 16:20:51 +0900

    fix: UserInfo 관련 정리 - 코드 정리 작업 실시 - react-Query를 이용하여 사용자 정보 입력하고, 사용자 정보에 관련하여 실시간 출력으로 구현 - 사용자 정보에 사용할 Typescript 용 Interface 설정 (TypeScript에 알맞게 작성함) - JS 기법으로 사용 시 Type 에러 발생하는 것을 방지

--- food-front/src/recoil/state/userBankInfoState.tsx
+++ food-front/src/recoil/state/userBankInfoState.tsx
@@ -1,11 +1,27 @@
 import {atom} from 'recoil'
 
+export interface UserBankInfoVo {
+  // userInfoVo에 담을 변수
+  uno: Number,
+  userMoney: Number,
+  pageNo: Number,
+  pageEventTitle: String,
+  pageEventView: String,
+  crtTime: String
+}
+
+export interface UserBankInfoType {
+  bno: Number
+  ,userMoney: Number
+};
+
+
 export const userBankInfoState = atom({
   key: 'userBankInfo',
   default: {
-      bno: 1
-    , userMoney: 200000
-  }
+      bno: 0
+    , userMoney: 0
+  } as UserBankInfoType
 });
 
 // https://velog.io/@juno7803/Recoil-Recoil-200-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0
--- food-front/src/recoil/state/userInfoState.tsx
+++ food-front/src/recoil/state/userInfoState.tsx
@@ -1,16 +1,43 @@
 import {atom} from 'recoil'
 
+
+export interface UserInfoVo {
+  // userInfoVo에 담을 변수
+  uno: Number,
+  userName: String,
+  userAge: Number,
+  userSex: String
+  userPhone: String,
+  userNumber: String,
+  userCardPassword: String,
+  lno: Number,
+  pageNo: Number,
+  pageEventTitle: String,
+  pageEventView: String,
+  crtTime: String
+}
+
+export interface UserInfoType {
+  uno: 0
+  , userName: ''
+  , userAge: 0
+  , userSex: ''
+  , userPhone: ''
+  , userNumber: ''
+  , userPassword: ''
+};
+
 export const userInfoState = atom({
   key: 'userInfo',
   default: {
-      uno: 1
-    , userName: 'berkley'
-    , userAge: 29
-    , userSex: '남'
-    , userPhone: '01011111111'
-    , userInfoNumber: '841111'
-    , userPassword: '1111'
-  }
+      uno: 0
+    , userName: ''
+    , userAge: 0
+    , userSex: ''
+    , userPhone: ''
+    , userNumber: ''
+    , userPassword: ''
+  } as UserInfoType
 });
 
 // https://velog.io/@juno7803/Recoil-Recoil-200-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0
