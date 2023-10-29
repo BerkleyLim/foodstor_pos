563f7e9818c4cd811e6ae869b35faf30c9cd3310
berkleylim <berkleylim16@gmail.com>
2023-10-29 17:58:27 +0900

    fix: 뒤로가기 시 로그아웃처럼 기능 API 연동 - 모든 정보 삭제하는 API로 구성 - 현재, 이것은 Toy Project이기 때문에 연습용으로 진행함

--- food-front/src/component/menu/MenuIndex.tsx
+++ food-front/src/component/menu/MenuIndex.tsx
@@ -6,24 +6,47 @@ import { useResetRecoilState } from 'recoil'
 import { userInfoState } from '../../recoil/state/userInfoState'
 import { logInfoState } from '../../recoil/state/logInfoState'
 import { userBankInfoState } from '../../recoil/state/userBankInfoState'
+import { useMutation } from 'react-query'
+
+// // 유저 정보 제거 - 이부분은 예시로 놔둠
 
 const MenuIndex = () => {
   const navigate = useNavigate();
-  
+
   // default 값으로 reset용
   const resetUserInfo = useResetRecoilState(userInfoState);
   const resetLogInfo = useResetRecoilState(logInfoState);
   const resetUserBankInfo = useResetRecoilState(userBankInfoState);
   
+  // 리액트 쿼리로 남김
+  // 유저 정보 제거
+  const deleteUserInfo = useMutation(async (removeData) => {
+    const response = await fetch('http://localhost:8080/api/user/info/delete/user/info', {
+      method: 'POST',
+      body: JSON.stringify(removeData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    return response;
+  }, {
+    onSuccess: () => {
+      resetUserInfo();
+      resetUserBankInfo();
+      resetLogInfo();
+    }
+  });
+
+
   const pageMoveClick = (url:any) => {
-    // Partial<Path> path = new Partial<>(url);
     if (url === '/inputuserinfo') {
       // recoil state 값 default 초기화
-      resetUserInfo();
+      // 유저 정보 초기화
       // 계좌 초기화
-      resetUserBankInfo();
       // 로그 초기화
-      resetLogInfo();
+      deleteUserInfo.mutate()
+      
     }
     navigate(url);
   }
